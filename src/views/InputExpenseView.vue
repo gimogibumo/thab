cc7c6cbc7cde8120075aa873e4ea83cb9ef9d119
Jinwook Lee <gfdsa9497@naver.com>
2025-04-09 18:40:42 +0900

    Design : 지출 입력 CSS 적용
    지출 입력 CSS 적용

--- src/views/InputExpenseView.vue
+++ src/views/InputExpenseView.vue
@@ -4,9 +4,23 @@ import { useRouter } from 'vue-router'
 import axios from 'axios'
 import { convertCurrency } from '@/utils/exchangeConverter'
 
+// 10000000000000000
 const router = useRouter()
 const categories = ['숙박', '식비', '교통', '관광', '쇼핑', '기타']
 const showCategories = ref(false)
+const currencyJson = ref(null)
+const showCurrencyDropdown = ref(false)
+const selectedCurrency = ref('')
+const moneyByWon = ref('')
+
+function toggleCurrencyDropdown() {
+  showCurrencyDropdown.value = !showCurrencyDropdown.value
+}
+
+function selectCurrency(currency) {
+  selectedCurrency.value = currency
+  showCurrencyDropdown.value = false
+}
 
 function toggleCategories() {
   showCategories.value = !showCategories.value
@@ -23,12 +37,18 @@ const expense = reactive({
 })
 
 const travels = ref([])
-const selectedTravel = ref()
+const selectedTravel = ref('')
 
 onMounted(async () => {
   try {
     const res = await axios.get('http://localhost:3000/travels')
     travels.value = res.data
+  } catch (err) {
+    console.error(err)
+  }
+  try {
+    const response = await fetch('/exchangeRates.json')
+    currencyJson.value = await response.json()
   } catch (err) {
     console.error(err)
   }
@@ -39,6 +59,16 @@ watch(selectedTravel, (newTravel) => {
     expense.inputName = newTravel.title
     expense.travelsKey = newTravel.id
     expense.date = ''
+  }
+})
+watch([() => expense.amount, selectedCurrency], ([newAmount, newCurrency]) => {
+  if (newCurrency && currencyJson.value) {
+    const rateObj = currencyJson.value.find((item) => item.cur_unit === newCurrency)
+    if (rateObj) {
+      const rate = parseFloat(rateObj.deal_bas_r.replace(',', ''))
+      moneyByWon.value = (newAmount ? newAmount * rate : 0).toFixed(2)
+      moneyByWon.value = parseInt(moneyByWon.value)
+    }
   }
 })
 
@@ -58,7 +88,11 @@ async function submitForm() {
   if (!expense.expenseName) return alert('지출 항목을 입력해주세요!')
   if (!expense.category) return alert('카테고리를 선택해주세요!')
   if (!expense.date) return alert('여행 날짜를 선택해주세요!')
+  if (expense.amount === 0) return alert('금액을 입력해주세요!')
 
+  if (moneyByWon.value) {
+    expense.amount = moneyByWon.value
+  }
   try {
     await axios.post('http://localhost:3000/expense', expense)
     alert('지출 내역이 저장되었습니다!')
@@ -84,8 +118,13 @@ function cancelForm() {
           <div class="selectTravel">
             <label for="inputName">여행 선택</label>
             <select id="inputName" v-model="selectedTravel">
-              <option selected hidden value="undefined">여행을 선택하세요</option>
-              <option v-for="travel in travels" :key="travel.id" :value="travel">
+              <option selected hidden value="">여행을 선택하세요</option>
+              <option
+                v-for="travel in travels"
+                :key="travel.id"
+                :value="travel"
+                style="overflow-x: auto; white-space: nowrap"
+              >
                 {{ travel.title }}
               </option>
             </select>
@@ -110,6 +149,7 @@ function cancelForm() {
               id="expenseName"
               v-model="expense.expenseName"
               placeholder="지출 항목을 입력해주세요"
+              style="overflow-x: auto; white-space: nowrap"
             />
           </div>
 
@@ -145,12 +185,48 @@ function cancelForm() {
               :min="1"
               step="1"
               @input="validateAmount"
+              style="overflow-x: auto; white-space: nowrap"
+              :disabled="!selectedCurrency"
             />
+          </div>
+          <div class="selectCurrency">
+            <label></label>
+            <div class="currencyBar" @click="toggleCurrencyDropdown">
+              <span>{{ selectedCurrency || '선택' }}</span>
+              <button type="button" class="currencyButton" :class="{ open: showCurrencyDropdown }">
+                ▼
+              </button>
+            </div>
+            <transition name="dropdown">
+              <div v-if="showCurrencyDropdown" class="currency-dropdown">
+                <button
+                  v-for="currency in currencyJson"
+                  :key="currency.cur_unit"
+                  type="button"
+                  class="currency-option"
+                  @click="selectCurrency(currency.cur_unit)"
+                >
+                  {{ currency.cur_unit }}
+                </button>
+              </div>
+            </transition>
+          </div>
+
+          <div class="resultWonContainer">
+            <label>KRW</label>
+            <div class="resultWon" style="overflow-x: auto; white-space: nowrap">
+              {{ moneyByWon ? moneyByWon + ' 원' : '' }}
+            </div>
           </div>
 
           <div class="inputMemo">
             <label for="memo">메모</label>
-            <input type="text" id="memo" v-model="expense.memo" />
+            <input
+              type="text"
+              id="memo"
+              v-model="expense.memo"
+              style="overflow-y: auto; resize: none"
+            />
           </div>
         </div>
 
@@ -169,11 +245,6 @@ h1 {
   font-weight: bold;
   margin-bottom: 2rem;
   color: #222;
-}
-
-.main {
-  margin: 0 auto;
-  width: 60%;
 }
 
 .container {
@@ -254,7 +325,7 @@ h1 {
 }
 
 .selectAccount {
-  width: 55%;
+  width: 50%;
 }
 
 .category-buttons {
@@ -264,7 +335,6 @@ h1 {
   width: 100%;
   grid-template-columns: repeat(3, 1fr);
   gap: 10px;
-  /* margin-top: 10px; */
   background: white;
   border: 1px solid #ddd;
   border-radius: 10px;
@@ -312,7 +382,6 @@ h1 {
 .categoryButton.open {
   transform: rotate(180deg);
 }
-/* 드롭다운 트랜지션 */
 .dropdown-enter-active,
 .dropdown-leave-active {
   transition: all 0.3s ease;
@@ -335,5 +404,76 @@ option {
 }
 .content {
   padding: 3% 5%;
+}
+.selectCurrency {
+  width: 13%;
+  position: relative;
+}
+
+.currencyBar {
+  width: 100%;
+  height: 48px;
+  border: 1px solid #ddd;
+  border-radius: 0.3rem;
+  padding: 0 15px;
+  margin: 5px 0;
+  background: #f9f9f9;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+}
+
+.currencyButton {
+  background: none;
+  border: none;
+  font-size: 1.2rem;
+  transform: rotate(0deg);
+  transition: transform 0.3s;
+}
+.currencyButton.open {
+  transform: rotate(180deg);
+}
+
+.currency-dropdown {
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  background: white;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  max-height: 200px;
+  overflow-y: auto;
+  z-index: 10;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+}
+
+.currency-option {
+  width: 100%;
+  height: 48px;
+  padding: 10px;
+  border: none;
+  background: white;
+  text-align: left;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background 0.2s;
+}
+
+.currency-option:hover {
+  background: #f0f0f0;
+}
+.resultWonContainer {
+  width: 32%;
+  margin-left: auto;
+}
+.resultWon {
+  width: 100%;
+  padding: 12px 16px;
+  background-color: #f5f5f5;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  color: #333;
+  height: 50px;
 }
 </style>
