14627b27318ef1a910ccbf44cec27ca43009c7ef
gimogibumo <wolfbone0304@gmail.com>
2025-04-11 00:26:16 +0900

    feature : 알림설정

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
